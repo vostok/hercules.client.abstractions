6459806f3bb5472cd54093f77cbd52d30a9d811a
aa <aa@skbkontur.ru>
2018-08-13 21:29:25 +0500

    Renamed parameter from values to value

--- Vostok.Airlock.Client.Abstractions/IAirlockRecordBuilder.cs
+++ Vostok.Airlock.Client.Abstractions/IAirlockRecordBuilder.cs
@@ -13,13 +13,13 @@ namespace Vostok.Airlock.Client.Abstractions
         IAirlockRecordBuilder Add(string key, float value);
         IAirlockRecordBuilder Add(string key, double value);
         IAirlockRecordBuilder Add(string key, string value);
-        IAirlockRecordBuilder Add(string key, byte[] values);
-        IAirlockRecordBuilder Add(string key, short[] values);
-        IAirlockRecordBuilder Add(string key, int[] values);
-        IAirlockRecordBuilder Add(string key, long[] values);
-        IAirlockRecordBuilder Add(string key, bool[] values);
-        IAirlockRecordBuilder Add(string key, float[] values);
-        IAirlockRecordBuilder Add(string key, double[] values);
-        IAirlockRecordBuilder Add(string key, string[] values);
+        IAirlockRecordBuilder Add(string key, byte[] value);
+        IAirlockRecordBuilder Add(string key, short[] value);
+        IAirlockRecordBuilder Add(string key, int[] value);
+        IAirlockRecordBuilder Add(string key, long[] value);
+        IAirlockRecordBuilder Add(string key, bool[] value);
+        IAirlockRecordBuilder Add(string key, float[] value);
+        IAirlockRecordBuilder Add(string key, double[] value);
+        IAirlockRecordBuilder Add(string key, string[] value);
     }
 }
