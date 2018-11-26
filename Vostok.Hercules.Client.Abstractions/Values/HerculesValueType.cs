aee26bf93ab3469085487d123c4f9ab405295e8e
iloktionov <iloktionov@skbkontur.ru>
2018-11-26 19:22:38 +0500

    HerculesValueType: added missing values and xml-doc.

--- Vostok.Hercules.Client.Abstractions/Values/HerculesValueType.cs
+++ Vostok.Hercules.Client.Abstractions/Values/HerculesValueType.cs
@@ -2,15 +2,23 @@
 
 namespace Vostok.Hercules.Client.Abstractions.Values
 {
+    /// <summary>
+    /// <para>Represents all known types of values in Hercules tags.</para>
+    /// <para>Note that numeric values of this enum do not correspond to values expected in binary format by Hercules.</para>
+    /// </summary>
     [PublicAPI]
     public enum HerculesValueType
     {
+        Byte,
+        Short,
         Int,
         Long,
+        Bool,
+        Float,
+        Double,
         Guid,
         String,
         Array,
         Container
-        // ...
     }
 }
