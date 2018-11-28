c7aa1f7d4e2c6f574d3e61d62a7036ec1c6e81c9
iloktionov <iloktionov@skbkontur.ru>
2018-11-28 17:11:02 +0500

    Implemented HerculesTags.

--- Vostok.Hercules.Client.Abstractions/Events/HerculesTags.cs
+++ Vostok.Hercules.Client.Abstractions/Events/HerculesTags.cs
@@ -1,15 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using Vostok.Hercules.Client.Abstractions.Values;
 
 namespace Vostok.Hercules.Client.Abstractions.Events
 {
+    /// <summary>
+    /// Represents an immutable key-value collection of tags, mapping case-sensitive string keys to <see cref="HerculesValue"/>s.
+    /// </summary>
     [PublicAPI]
-    public class HerculesTags
+    public class HerculesTags : IEnumerable<KeyValuePair<string, HerculesValue>>
     {
-        // TODO(iloktionov): convenient indexer
+        public static readonly HerculesTags Empty = new HerculesTags(new Dictionary<string, HerculesValue>());
 
-        // TODO(iloktionov): rename or hide property
-        public IReadOnlyDictionary<string, HerculesValue> Tags { get; }
+        private readonly IReadOnlyDictionary<string, HerculesValue> tags;
+
+        public HerculesTags([NotNull] IReadOnlyDictionary<string, HerculesValue> tags)
+        {
+            this.tags = tags;
+        }
+
+        public bool ContainsKey([NotNull] string key) => tags.ContainsKey(key);
+
+        public bool TryGetValue([NotNull] string key, out HerculesValue value) => tags.TryGetValue(key, out value);
+
+        public IEnumerator<KeyValuePair<string, HerculesValue>> GetEnumerator() => tags.GetEnumerator();
+
+        /// <summary>
+        /// <para>Returns the value of tag with given <paramref name="key"/> or <c>null</c> if no such tag exists.</para>
+        /// <para>Does not throw exceptions.</para>
+        /// </summary>
+        [CanBeNull]
+        public HerculesValue this[[NotNull] string key] => tags.TryGetValue(key, out var value) ? value : null;
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
