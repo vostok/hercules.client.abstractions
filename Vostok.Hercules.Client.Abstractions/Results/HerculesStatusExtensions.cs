7c26a57357864c24a2e3bc001be95dd44ca38650
iloktionov <iloktionov@skbkontur.ru>
2018-12-01 14:17:13 +0500

    HerculesStatus: added IsSuccessful() and EnsureSuccess() extensions.

---
+++ Vostok.Hercules.Client.Abstractions/Results/HerculesStatusExtensions.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Hercules.Client.Abstractions.Results
+{
+    [PublicAPI]
+    public static class HerculesStatusExtensions
+    {
+        /// <summary>
+        /// Returns <c>true</c> if given <paramref name="status"/> is <see cref="HerculesStatus.Success"/> or <c>false</c> otherwise.
+        /// </summary>
+        public static bool IsSuccessful(this HerculesStatus status)
+        {
+            return status == HerculesStatus.Success;
+        }
+
+        /// <summary>
+        /// Throws a <see cref="HerculesException"/> if given <paramref name="status"/> is not <see cref="IsSuccessful"/>.
+        /// </summary>
+        public static void EnsureSuccess(this HerculesStatus status)
+        {
+            if (IsSuccessful(status))
+                throw new HerculesException(status);
+        }
+    }
+}
