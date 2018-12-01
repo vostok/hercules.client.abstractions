396ae4c770bf83f56edfe65ef5721313d3788ad1
iloktionov <iloktionov@skbkontur.ru>
2018-12-01 14:35:33 +0500

    + DevNullHerculesSink

---
+++ Vostok.Hercules.Client.Abstractions/DevNullHerculesSink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Events;
+
+namespace Vostok.Hercules.Client.Abstractions
+{
+    /// <summary>
+    /// An implementation of <see cref="IHerculesSink"/> that simply drops all incoming events.
+    /// </summary>
+    [PublicAPI]
+    public class DevNullHerculesSink : IHerculesSink
+    {
+        public void Put(string streamName, Action<IHerculesEventBuilder> buildEvent)
+        {
+        }
+    }
+}
