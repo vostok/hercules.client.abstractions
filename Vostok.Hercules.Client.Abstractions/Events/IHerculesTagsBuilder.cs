4c71c73af0389b00a442891e67e101a9ad9c47ff
epeshk <peshkov@skbkontur.ru>
2018-12-07 15:34:15 +0500

    move Add methods for byte to IHerculesTags.Boilerplate file

--- Vostok.Hercules.Client.Abstractions/Events/IHerculesTagsBuilder.cs
+++ Vostok.Hercules.Client.Abstractions/Events/IHerculesTagsBuilder.cs
@@ -8,20 +8,6 @@ namespace Vostok.Hercules.Client.Abstractions.Events
     public partial interface IHerculesTagsBuilder
     {
         /// <summary>
-        /// <para>Adds a scalar tag with given <paramref name="key"/> and <paramref name="value"/> to the tags collection.</para>
-        /// <para>Conflict resolution (behaviour when a tag with such name already exists) is implementation-specific.</para>
-        /// <para>Returned value is utilized solely for the purpose of fluent syntax (chaining calls) and should not return a different instance of <see cref="IHerculesTagsBuilder"/>.</para>
-        /// </summary>
-        IHerculesTagsBuilder AddValue([NotNull] string key, byte value);
-
-        /// <summary>
-        /// <para>Adds a primitive array tag with given <paramref name="key"/> and <paramref name="values"/> to the tags collection.</para>
-        /// <para>Conflict resolution (behaviour when a tag with such name already exists) is implementation-specific.</para>
-        /// <para>Returned value is utilized solely for the purpose of fluent syntax (chaining calls) and should not return a different instance of <see cref="IHerculesTagsBuilder"/>.</para>
-        /// </summary>
-        IHerculesTagsBuilder AddVector([NotNull] string key, [NotNull] IReadOnlyList<byte> values);
-
-        /// <summary>
         /// <para>Adds a container tag with given <paramref name="key"/> and contents built using given <paramref name="valueBuilder"/> delegate.</para>
         /// <para>Conflict resolution (behaviour when a tag with such name already exists) is implementation-specific.</para>
         /// <para>Returned value is utilized solely for the purpose of fluent syntax (chaining calls) and should not return a different instance of <see cref="IHerculesTagsBuilder"/>.</para>
