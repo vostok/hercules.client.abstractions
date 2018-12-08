608babe284d88c7e3cc8ae6cc58f42a3c8e44759
iloktionov <iloktionov@skbkontur.ru>
2018-12-08 20:14:09 +0500

    HerculesValueOfT no longer delegates unwrapping to HerculesValueHelpers.

--- Vostok.Hercules.Client.Abstractions/Values/HerculesValueOfT.cs
+++ Vostok.Hercules.Client.Abstractions/Values/HerculesValueOfT.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 
 // ReSharper disable PossibleInvalidOperationException
 
@@ -8,20 +9,31 @@ namespace Vostok.Hercules.Client.Abstractions.Values
     internal class HerculesValue<TValue> : HerculesValue
     {
         private static readonly HerculesValueType? type = HerculesValueHelpers.TryMapToHerculesType(typeof(TValue));
-        
+
         public HerculesValue(TValue value)
         {
             if (type == null)
                 HerculesValueHelpers.ThrowNotSupportedException(typeof(TValue));
-            
+
             TypedValue = value;
         }
 
         public TValue TypedValue { get; }
 
         public override HerculesValueType Type => type.Value;
-        
-        public override object Value => HerculesValueHelpers.UnwrapValue(this);
-    }
 
+        public override object Value
+        {
+            get
+            {
+                if (IsNull)
+                    return null;
+
+                if (IsVector)
+                    return AsVector.Elements.Select(element => element.Value);
+
+                return TypedValue;
+            }
+        }
+    }
 }
