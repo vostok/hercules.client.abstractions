5d2663a205ef0579a4ddfe92e9d37ae45db88434
iloktionov <iloktionov@skbkontur.ru>
2019-03-05 23:20:06 +0500

    HerculesNull: implemented Equals.

--- Vostok.Hercules.Client.Abstractions/Values/HerculesNull.cs
+++ Vostok.Hercules.Client.Abstractions/Values/HerculesNull.cs
@@ -1,10 +1,22 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 
 namespace Vostok.Hercules.Client.Abstractions.Values
 {
     [PublicAPI]
-    public class HerculesNull
+    public class HerculesNull : IEquatable<HerculesNull>
     {
         public static readonly HerculesNull Instance = new HerculesNull();
+
+        #region Equality
+
+        public bool Equals(HerculesNull other) => true;
+
+        public override bool Equals(object other)
+            => other is HerculesNull;
+
+        public override int GetHashCode() => 0;
+
+        #endregion
     }
 }
