3bc559e864e0bbde230890c44296ac49eb757816
iloktionov <iloktionov@skbkontur.ru>
2019-03-05 23:11:13 +0500

    HerculesEvent: implemented Equals.

--- Vostok.Hercules.Client.Abstractions/Events/HerculesEvent.cs
+++ Vostok.Hercules.Client.Abstractions/Events/HerculesEvent.cs
@@ -8,7 +8,7 @@ namespace Vostok.Hercules.Client.Abstractions.Events
     /// <para>Every event consists of a <see cref="Timestamp"/> and a collection of <see cref="Tags"/> (see <see cref="HerculesTags"/> for more info).</para>
     /// </summary>
     [PublicAPI]
-    public class HerculesEvent
+    public class HerculesEvent : IEquatable<HerculesEvent>
     {
         public HerculesEvent(DateTimeOffset timestamp, [NotNull] HerculesTags tags)
         {
@@ -20,5 +20,31 @@ namespace Vostok.Hercules.Client.Abstractions.Events
 
         [NotNull]
         public HerculesTags Tags { get; }
+
+        #region Equality
+
+        public bool Equals(HerculesEvent other)
+        {
+            if (other == null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            return Timestamp.UtcTicks == other.Timestamp.UtcTicks && Tags.Equals(other.Tags);
+        }
+
+        public override bool Equals(object other)
+            => Equals(other as HerculesEvent);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Timestamp.UtcTicks.GetHashCode() * 397) ^ Tags.GetHashCode();
+            }
+        }
+
+        #endregion
     }
 }
