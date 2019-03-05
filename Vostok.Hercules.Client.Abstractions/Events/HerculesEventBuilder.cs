2a1551b105acb0070bed2994c391660775f3418b
epeshk <peshkov@skbkontur.ru>
2019-03-05 14:49:01 +0500

    Added docs for IHerculesEventBuilder.

--- Vostok.Hercules.Client.Abstractions/Events/HerculesEventBuilder.cs
+++ Vostok.Hercules.Client.Abstractions/Events/HerculesEventBuilder.cs
@@ -8,8 +8,10 @@ namespace Vostok.Hercules.Client.Abstractions.Events
     {
         private DateTimeOffset eventTimestamp;
 
+        /// <returns>Resulting <see cref="HerculesEvent"/></returns>
         public HerculesEvent BuildEvent() => new HerculesEvent(eventTimestamp, BuildTags());
 
+        /// <inheritdoc />
         public IHerculesEventBuilder SetTimestamp(DateTimeOffset timestamp)
         {
             eventTimestamp = timestamp;
--- Vostok.Hercules.Client.Abstractions/Events/IHerculesEventBuilder.cs
+++ Vostok.Hercules.Client.Abstractions/Events/IHerculesEventBuilder.cs
@@ -6,6 +6,10 @@ namespace Vostok.Hercules.Client.Abstractions.Events
     [PublicAPI]
     public interface IHerculesEventBuilder : IHerculesTagsBuilder
     {
+        /// <summary>
+        /// <para>Sets a <paramref name="timestamp"/> that will be used in resulting <see cref="HerculesEvent"/>.</para>
+        /// <para><paramref name="timestamp"/> should be greater than 1st Jan 1970 in UTC (unix epoch start).</para>
+        /// </summary>
         [NotNull]
         IHerculesEventBuilder SetTimestamp(DateTimeOffset timestamp);
     }
