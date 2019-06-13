b736722f8e77b87dcd9e92b00b7a4e135c720ff3
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-13 14:16:19 +0500

    move TagType to abstractions

--- Vostok.Hercules.Client.Abstractions/Events/IHerculesEventsBinaryReader.cs
+++ Vostok.Hercules.Client.Abstractions/Events/IHerculesEventsBinaryReader.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections.Generic;
 using JetBrains.Annotations;
 
-namespace Vostok.Hercules.Client.Abstractions
+namespace Vostok.Hercules.Client.Abstractions.Events
 {
     /// <summary>
     /// Provides a way to customize reading of hercules events.
     /// </summary>
     [PublicAPI]
-    public interface IEventsBinaryReader<T>
+    public interface IHerculesEventsBinaryReader<T>
     {
         IList<T> Read(byte[] bytes, int offset);
     }
