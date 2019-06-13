e3c641ba5358c785890bf7f07deda542aa644c9e
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-13 15:48:56 +0500

    Revert "move TagType to abstractions"
    This reverts commit b736722f8e77b87dcd9e92b00b7a4e135c720ff3.

--- Vostok.Hercules.Client.Abstractions/IEventsBinaryReader.cs
+++ Vostok.Hercules.Client.Abstractions/IEventsBinaryReader.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections.Generic;
 using JetBrains.Annotations;
 
-namespace Vostok.Hercules.Client.Abstractions.Events
+namespace Vostok.Hercules.Client.Abstractions
 {
     /// <summary>
     /// Provides a way to customize reading of hercules events.
     /// </summary>
     [PublicAPI]
-    public interface IHerculesEventsBinaryReader<T>
+    public interface IEventsBinaryReader<T>
     {
         IList<T> Read(byte[] bytes, int offset);
     }
