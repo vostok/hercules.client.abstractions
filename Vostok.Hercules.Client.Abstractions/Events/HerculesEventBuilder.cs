0933ab84ce5f09fc071caa69afbffd4840016ae5
kungurtsev <kungurtsev@skbkontur.ru>
2019-06-19 16:48:13 +0500

    return back old HerculesEventBuilder

--- Vostok.Hercules.Client.Abstractions/Events/HerculesEventBuilder.cs
+++ Vostok.Hercules.Client.Abstractions/Events/HerculesEventBuilder.cs
@@ -4,7 +4,7 @@ using JetBrains.Annotations;
 namespace Vostok.Hercules.Client.Abstractions.Events
 {
     [PublicAPI]
-    public class HerculesEventBuilder : HerculesTagsBuilder, IHerculesEventBuilder, IHerculesEventBuilder<HerculesEvent>
+    public class HerculesEventBuilder : HerculesTagsBuilder, IHerculesEventBuilder
     {
         private DateTimeOffset eventTimestamp;
 
@@ -21,15 +21,7 @@ namespace Vostok.Hercules.Client.Abstractions.Events
         /// <returns>Resulting <see cref="HerculesEvent"/></returns>
         public HerculesEvent BuildEvent() => new HerculesEvent(eventTimestamp, BuildTags());
 
-        /// <inheritdoc />
-        public IHerculesEventBuilder<HerculesEvent> SetTimestamp(DateTimeOffset timestamp)
-        {
-            eventTimestamp = timestamp;
-            return this;
-        }
-
-        /// <inheritdoc />
-        IHerculesEventBuilder IHerculesEventBuilder.SetTimestamp(DateTimeOffset timestamp)
+        public IHerculesEventBuilder SetTimestamp(DateTimeOffset timestamp)
         {
             eventTimestamp = timestamp;
             return this;
--- Vostok.Hercules.Client.Abstractions/IHerculesStreamClientExtensions.cs
+++ Vostok.Hercules.Client.Abstractions/IHerculesStreamClientExtensions.cs
@@ -26,30 +26,5 @@ namespace Vostok.Hercules.Client.Abstractions
             TimeSpan timeout,
             CancellationToken cancellationToken = default) =>
             client.SeekToEndAsync(query, timeout, cancellationToken).GetAwaiter().GetResult();
-
-        [NotNull]
-        public static IHerculesStreamClient<HerculesEvent> ToGenericClient([NotNull] this IHerculesStreamClient client)
-        {
-            return new GenericAdapter(client);
-        }
-
-        private class GenericAdapter : IHerculesStreamClient<HerculesEvent>
-        {
-            private readonly IHerculesStreamClient client;
-
-            public GenericAdapter(IHerculesStreamClient client)
-            {
-                this.client = client;
-            }
-
-            public async Task<ReadStreamResult<HerculesEvent>> ReadAsync(ReadStreamQuery query, TimeSpan timeout, CancellationToken cancellationToken = default)
-            {
-                var result = await client.ReadAsync(query, timeout, cancellationToken).ConfigureAwait(false);
-                return result.ToGenericResult();
-            }
-
-            public Task<SeekToEndStreamResult> SeekToEndAsync(SeekToEndStreamQuery query, TimeSpan timeout, CancellationToken cancellationToken = default) =>
-                client.SeekToEndAsync(query, timeout, cancellationToken);
-        }
     }
 }
