a8cdda01a2060316df708856576d379645a65e1f
kungurtsev <kungurtsev@skbkontur.ru>
2020-03-11 19:35:56 +0500

    + FromGenericResult

---
+++ Vostok.Hercules.Client.Abstractions/Results/ReadStreamIEnumerableResultExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Vostok.Hercules.Client.Abstractions.Events;
+
+namespace Vostok.Hercules.Client.Abstractions.Results
+{
+    [PublicAPI]
+    public static class ReadStreamIEnumerableResultExtensions
+    {
+        public static ReadStreamIEnumerableResult FromGenericResult(this ReadStreamIEnumerableResult<HerculesEvent> result)
+        {
+            if (result == null)
+                return null;
+
+            return result.IsSuccessful
+                ? new ReadStreamIEnumerableResult(result.Status, 
+                    new ReadStreamIEnumerablePayload(result.Payload.Events, result.Payload.Current, result.Payload.Next, () => result.Payload.Dispose()))
+                : new ReadStreamIEnumerableResult(result.Status, null, result.ErrorDetails);
+        }
+    }
+}
